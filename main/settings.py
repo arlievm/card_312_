2640bbde0615466442d325dcb50285fc581ba6c6
newse <muhamed2000kg@mail.rum>
2023-06-05 15:30:13 +0600

    fix updates

--- main/settings.py
+++ main/settings.py
@@ -1,5 +1,5 @@
 import os
-import django_heroku
+# import django_heroku
 from pathlib import Path
 
 
@@ -176,4 +176,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
