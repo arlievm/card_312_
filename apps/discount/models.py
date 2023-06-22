835f37ecb87d44bc5290c89cbe8a4abbf4594554
newse <muhamed2000kg@mail.rum>
2023-06-22 13:28:06 +0600

    fix deploy

--- apps/discount/models.py
+++ apps/discount/models.py
@@ -9,3 +9,6 @@ class Discounts(models.Model):
     image = models.ImageField(verbose_name="ФОТОГРАФИЯ *(387x167)", upload_to="apps/discount/images/")
     name = models.CharField(verbose_name="Наименование", max_length=50)
     description = models.TextField(verbose_name='Описание');
+    
+    def __str__(self):
+        return self.name
