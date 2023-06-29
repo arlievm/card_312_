d65204f204c7f39b0d120c56c1d5cb7dcd900600
newse <muhamed2000kg@mail.rum>
2023-06-29 12:58:39 +0600

    fix chenge settings

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -11,6 +11,7 @@ class Contacts(models.Model):
     name = models.CharField(verbose_name='Ф.И.О',max_length=64)
     phone = models.CharField(verbose_name='Телефон',max_length=36)
     email = models.EmailField(verbose_name='Почта',max_length=64)
+    email_e = models.EmailField(verbose_name='Почта2',max_length=64)
 
     def __str__(self):
         return self.name
