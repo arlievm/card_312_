be35bf30a262cb91d6f36960d6410b00dfa063ed
newse <muhamed2000kg@mail.rum>
2023-06-29 12:51:12 +0600

    fix chenge settings

--- main/settings.py
+++ main/settings.py
@@ -104,8 +104,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # }
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'card_db',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
        'USER': 'card_user',
        'PASSWORD': 'card_123',
        'HOST': 'pgdb',
