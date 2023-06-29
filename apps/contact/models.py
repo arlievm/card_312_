730b9353c876f838b024b21dd788c7e9c36085e4
newse <muhamed2000kg@mail.rum>
2023-06-29 11:53:10 +0600

    fix chenge models

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -11,11 +11,6 @@ class Contacts(models.Model):
     name = models.CharField(verbose_name='Ф.И.О',max_length=64)
     phone = models.CharField(verbose_name='Телефон',max_length=36)
     email = models.EmailField(verbose_name='Почта',max_length=64)
-    email_e = models.EmailField(verbose_name='Почта 2',max_length=64)
-    email_r = models.EmailField(verbose_name='Почта 3',max_length=64)
-    email_ww = models.EmailField(verbose_name='Почта 4',max_length=64)
-    email_qw = models.EmailField(verbose_name='Почта 5',max_length=64)
-    email_qt = models.EmailField(verbose_name='Почта 6',max_length=64)
 
     def __str__(self):
         return self.name
