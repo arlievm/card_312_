de661f69be1d23e3c74f65baf37c1d57c5520a4b
newse <muhamed2000kg@mail.rum>
2023-06-29 14:19:36 +0600

    fix chenge settings

--- main/settings.py
+++ main/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-       'USER': 'card_user',
-       'PASSWORD': 'card_123',
-       'HOST': 'pgdb',
-       # 'HOST': 'ec2-18-234-17-166.compute-1.amazonaws.com',
-       'PORT': '',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'card_db',
+#        'USER': 'card_user',
+#        'PASSWORD': 'card_123',
+#        'HOST': 'pgdb',
+#        # 'HOST': 'ec2-18-234-17-166.compute-1.amazonaws.com',
+#        'PORT': '',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
